c5ff97ef971026de6e959796bb2f3e3c3606488e
Marcelo Molinas <federico.marcelo.molinas@gmail.com>
2021-09-02 14:04:34 -0300

    scss sass plugin add

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Tarjeta de estadisticas`,
+    description: `Tarjetasd de estadisticas de jugadores de rugby.`,
+    author: `@marcelo69420`,
+    siteUrl: `https://www.github.com/Marcelo-Molinas`,
   },
   plugins: [
+    `gatsby-plugin-sass`
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
